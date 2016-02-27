f0cdb1d7cc63f3826cfe69ac8429bf1da8087a11
Aris Abramian <aris@sataris.com.au>
2016-02-28 03:19:25 +0800

    initial commit of Sporty

---
+++ app/Http/Controllers/defaultController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class defaultController extends Controller
+{
+    //
+    //
+    
+    public function index(Request $request)
+    {
+        return response()->view('welcome');
+    }
+}
